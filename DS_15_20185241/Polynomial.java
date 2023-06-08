26e4315c3e362fdca2a6085331377c2cf1f2a5d9
BeomCheol Park <kawlao46@naver.com>
2023-06-08 09:03:53 +0900

    d
    d

---
+++ DS_15_20185241/Polynomial.java
@@ -0,0 +1,48 @@
+package poly3;
+
+public class Polynomial {
+	private PolyNode firstNode;
+	private PolyNode lastNode;
+	
+	public Polynomial() {
+		firstNode = null;
+		lastNode = null;
+	}
+	
+	public boolean ispZero() {
+
+	}
+	
+	public void addTerm(int c, int e) {
+
+	}
+	
+	public void delTerm(int e) {
+
+	}
+	
+	public Polynomial sMult(int c, int e) {
+
+	}
+	
+	public Polynomial polyMult(Polynomial q) {
+
+	}
+	
+	public void print() {
+		PolyNode p = firstNode;
+		while(p != null) {
+			System.out.print(p.coef + "x^" + p.exp + " + ");
+			p = p.link;
+		}
+		System.out.println();
+	}
+	
+	public void appendTerm(int c, int e) {
+
+	}
+	
+	public Polynomial polyAdd(Polynomial poly) {
+
+	}
+}
